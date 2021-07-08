2d3ffb95f6f17d8713867a35851021bf8c0b72ec
yunimua <8401051208+yunimua@users.noreply.github.com>
2021-07-09 00:14:05 +0900

    0708_E5_Decrypt수정

---
+++ JavaStudy/src/E05_Decrypt.java
@@ -0,0 +1,53 @@
+
+
+import java.io.*;
+import java.util.*;
+
+public class E05_Decrypt {
+
+	/*
+		사용자로부터 파일명과 열쇠값을 입력받으면
+		해당 열쇠값으로 복호화 한 결과물을 생성하는 프로그램을 만들어보세요.
+		
+		test. 암호화되어있는 frankenstein_encrypted.txt를 복호화 해보세요
+	
+	*/
+	public static void main(String[] args) {
+		
+		encryption("files/frankenstein_encrypted.txt");
+	}
+	
+	public static void encryption(String path) {
+		
+		ArrayList<String> lines = new ArrayList<>();
+		File target = new File(path);
+		
+		try (BufferedReader in = new BufferedReader(new FileReader(target));) {
+			String line;
+			while((line = in.readLine()) != null) {
+				lines.add(line);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		//System.out.printf("복호화 성공! [키: %d]\n" , key_value);
+		Scanner sc = new Scanner(System.in);
+		System.out.print("key 값을 입력 : ");
+		int key_value = sc.nextInt();
+		
+		for (String line : lines) {
+			for (char ch : line.toCharArray()) {
+				if (Character.isUpperCase(ch)) {
+					ch = (char) (ch - key_value < 'A' ? ch - key_value + 26 : ch - key_value); //A보다 작으면 +26을 더하고, 아니면 뺀걸 준다
+				}else if (Character.isLowerCase(ch)) {
+					ch = (char) (ch - key_value < 'a' ? ch - key_value + 26 : ch - key_value);
+				}
+				System.out.print(ch);
+			}
+			System.out.println();
+		}
+		System.out.printf("복호화 성공! [키: %d]\n" , key_value);
+	}
+}
---
+++ JavaStudy/src/E05_HackTool.java
@@ -0,0 +1,75 @@
+
+
+import java.io.*;
+import java.util.*;
+
+public class E05_HackTool {
+
+	/*
+		우리가 사용한 암호 방식은 키 값이 25개밖에 없는 허접한 암호화 방식입니다.
+		목표 파일의 맨 앞 부분 10줄을 1 ~ 25의 키로 모두 복호화 해보세요.
+	
+	*/
+	
+	public static void main(String[] args) {
+		bruteforce("files/frankenstein_encrypted.txt");
+	}
+	
+	public static void bruteforce(String path) {
+		ArrayList<String> lines = new ArrayList<>();
+		
+		File target = new File(path);
+		
+		try (
+		FileReader fin = new FileReader(target);
+		BufferedReader in = new BufferedReader(fin);
+		) {
+			
+			// 10번 또는 파일이 끝날 때까지 반복 : i가 10이면서 Line이 null이 아니어야 한다
+			String line;
+			for (int i = 0; i < 10 && (line = in.readLine()) != null; ++i) {
+				// 비어있는 줄 제외하고 꽉 찬 열줄을 보고 싶음
+				//+ if (line.trim().equals("")) {
+				
+				// 줄이 비어있거나 문자가 5개도 안되는 줄은 안본걸로 치겠다
+				if (line.trim().equals("") || line.length() < 5) {
+					--i;
+				}else {
+					lines.add(line);
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		Scanner sc = new Scanner(System.in);
+		
+		for (int key  = 1; key <26; ++key) {
+			System.out.printf("-----key: %d -------------------------------\n", key);
+			for (String line : lines) {
+				
+				for (char ch : line.toCharArray()) {
+					if (Character.isUpperCase(ch)) {
+						ch = (char) (ch - key < 'A' ? ch - key + 26 : ch - key); //A보다 작으면 +26을 더하고, 아니면 뺀걸 준다
+					}else if (Character.isLowerCase(ch)) {
+						ch = (char) (ch - key < 'a' ? ch - key + 26 : ch - key);
+					}
+					System.out.print(ch);
+				}
+				System.out.println();
+			}
+			System.out.println();
+			
+			System.out.println("Press 'Enter' to continue...");
+			String cmd = sc.nextLine();
+			
+			if (cmd.toLowerCase().equals("d")) {
+				System.out.println("전체 파일 복호화를 진행합니다..");
+				break;
+			}
+		}
+		System.out.println("프로그램 끝!!");
+	}
+}
